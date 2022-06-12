c0bff8c81b1e33a180f3a1dc08e9d410d89afe6e
Rob O'Leary <roboleary@anonymous.dev>
2022-06-13 00:54:56 +0300

    Added JSDoc to `markdown.js`

--- src/markdown.js
+++ src/markdown.js
@@ -1,3 +1,9 @@
+/**
+ * Check if a block of text contains a markdown horiztonal rule.
+ *
+ * @param {string} text A block of text
+ * @returns {boolean}
+ */
 function isHorizontalRule(text) {
   let str = text.replace(/\s/g, "");
 
